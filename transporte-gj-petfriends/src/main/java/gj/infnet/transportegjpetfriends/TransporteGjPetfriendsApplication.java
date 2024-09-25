f336d806a647b56d51c420759ad82a17f7b35ca3
Gabriel Justino Marques Couto <gabriel.justino@dsgtechnology.com.br>
2024-09-25 18:39:56 -0300

    cria transporte service

---
+++ transporte-gj-petfriends/src/main/java/gj/infnet/transportegjpetfriends/TransporteGjPetfriendsApplication.java
@@ -0,0 +1,13 @@
+package gj.infnet.transportegjpetfriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporteGjPetfriendsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransporteGjPetfriendsApplication.class, args);
+    }
+
+}
---
+++ transporte-gj-petfriends/src/test/java/gj/infnet/transportegjpetfriends/TransporteGjPetfriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package gj.infnet.transportegjpetfriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransporteGjPetfriendsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
